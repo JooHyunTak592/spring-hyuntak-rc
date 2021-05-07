74268a6f447f1025b44b0e53b0286a71a62a4d19
songk1992-home <zuppiy@naver.com>
2021-05-07 23:48:34 +0900

    음악기능 추가

--- src/main/resources/static/js/menu1.js
+++ src/main/resources/static/js/menu1.js
@@ -1,15 +1,16 @@
+const ctx = window.location.pathname.substring(0, window.location.pathname.indexOf("/",2));
+const pageLocation = location.href.substring(0, location.href.indexOf(ctx))
+const url = "music.mp3";
 
-
-
-
-
+audioObj = new Audio(url);
 
 function healingMusic(){
-	
+	console.log("h");
+	audioObj.play();
 }
 
 function logout(){
-	
+	window.location = pageLocation + ctx + "/"
 }
 
 
